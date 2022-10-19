c470f1594cb5f316c1c1807a68e84ec2e3c7d926
Ramish <agamatimtim@gmail.com>
2022-10-19 12:14:32 -0500

    all sorts implemented with read from console args

--- Sorts.py
+++ Sorts.py
@@ -1,6 +1,7 @@
+import sys
 # insertion sort printing after each iteration
 def insertion_sort(A):
-    print(A)
+    print(*A, sep=", ")
     for i in range(1, len(A)):
         key = A[i]
         j = i - 1
@@ -8,8 +9,8 @@ def insertion_sort(A):
             A[j + 1] = A[j]
             j -= 1
         A[j + 1] = key
-        print(A)
-    print(A)
+        print(*A, sep=", ")
+    print(*A, sep=", ")
 
 
 # selection sort printing after each iteration
@@ -21,9 +22,9 @@ def selection_sort(A):
             if A[j] < A[min]:
                 min = j
         A[i], A[min] = A[min], A[i]
-        if not copy.__eq__(A): print(copy)
-    print(A)
-    print(A)
+        if not copy.__eq__(A): print(*copy, sep=", ")
+    print(*A, sep=", ")
+    print(*A, sep=", ")
 
 
 # merge sort iteratively printing after each iteration
@@ -53,7 +54,7 @@ def merge_iterative(A, temp, frm, mid, to):
 
 # Iteratively sort sublist `A[low…high]` using a temporary list
 def mergesort_iterative(A):
-    print(A)
+    print(*A, sep=", ")
     low = 0
     high = len(A) - 1
     temp = A.copy()
@@ -65,13 +66,114 @@ def mergesort_iterative(A):
             to = min(i + 2 * m - 1, high)
             merge_iterative(A, temp, frm, mid, to)
         m = 2 * m
-    print(A)
+    print(*A, sep=", ")
 
 
+# merge sort recursively printing after each iteration
+# MergeSort in Python
+
+def merge_sort(arr, start, end):
+    if start < end:
+        mid = (start + end) // 2
+        merge_sort(arr, start, mid)
+        merge_sort(arr, mid + 1, end)
+        merge(arr, start, mid, end)
 
 
+def merge(arr, start, mid, end):
+    # creating a temp array
+    temp = [0] * (end - start + 1)
+    # initial indexes of first and second subarrays
+    i, j = start, mid + 1
+    # initial index of merged subarry array
+    k = 0
+    while i <= mid and j <= end:
+        if arr[i] <= arr[j]:
+            temp[k] = arr[i]
+            k += 1
+            i += 1
+        else:
+            temp[k] = arr[j]
+            k += 1
+            j += 1
+    # add remaining elements of the first subarray
+    while i <= mid:
+        temp[k] = arr[i]
+        k += 1
+        i += 1
+    # add remaining elements of the second subarray
+    while j <= end:
+        temp[k] = arr[j]
+        k += 1
+        j += 1
+    # copy temp array to the original array
+    for i in range(start, end + 1):
+        arr[i] = temp[i - start]
+    print(*arr, sep=", ")
 
-arr = [9, 3, 6, 4, 0, 1, 2, 7, 8, 0]
+
+# quick sort recursively printing after each iteration and choosing first element as pivot
+def quick_sort(arr, low, high):
+    if low < high:
+        pi = partition(arr, low, high)
+        quick_sort(arr, low, pi - 1)
+        quick_sort(arr, pi + 1, high)
+
+
+# quick sort partition with first element being the pivot and two pointers starting from low and high
+def partition(arr, low, high):
+    piv = arr[low]
+    while low < high:
+        while high > low:
+            if arr[high] < piv:
+                arr[low] = arr[high]
+                print(*arr, sep=", ")
+                break
+            else:
+                high -= 1
+        if low < high:
+            low += 1
+
+        while low < high:
+            if arr[low] > piv:
+                arr[high] = arr[low]
+                print(*arr, sep=", ")
+                break
+            else:
+                low += 1
+
+        arr[high] = piv
+    print(*arr, sep=", ")
+    return high
+
+    # save the first element arr[low]
+
+
+# arr = [9, 3, 6, 4, 0, 1, 2, 7, 8, 0]
 # insertion_sort(arr)
 # selection_sort(arr)
 # mergesort_iterative(arr)
+# merge_sort(arr, 0, len(arr) - 1)
+# quick_sort(arr, 0, len(arr) - 1)
+
+if __name__ == '__main__':
+    args = sys.argv[1:]
+    if len(args) == 0:
+        print("Please enter a list of numbers to sort")
+        sys.exit(1)
+    passed_args = [x for x in args]
+    filename, option = passed_args[0], passed_args[1]
+    print ( option)
+    file = open(filename, "r")
+    arr = [int(x) for x in file.readline().split(',') if x.isdigit()]
+    print(*arr, sep=", ")
+    if option == "1":
+        insertion_sort(arr)
+    elif option == "2":
+        selection_sort(arr)
+    elif option == "3":
+        quick_sort(arr, 0, len(arr) - 1)
+    elif option == "4":
+        merge_sort(arr, 0, len(arr) - 1)
+    print(*arr, sep=", ")
+pass
